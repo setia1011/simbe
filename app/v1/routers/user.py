fc39e620930757abe214fdeeb187f5eb8147ae4f
setiadi1 <edu.setiadi.my@gmail.com>
2024-01-25 21:52:52 +0700

    Update user with PATCH, partial update

--- app/v1/routers/user.py
+++ app/v1/routers/user.py
@@ -21,6 +21,10 @@ def users(db: Session = Depends(db_sync_session)):
 def user(t: sc_user.UserPost, db: Session = Depends(db_sync_session)):
    return sv_user.user_post(t=t, db=db)
 
+@router.patch("/user", name="Update a user", response_model=sc_user.User)
+def user(t: sc_user.UserPost, db: Session = Depends(db_sync_session)):
+   return sv_user.user_patch(t=t, db=db)
+
 @router.post("/user/role", name="Create a new user role", response_model=sc_user.Role)
 def role(t: sc_user.RolePost, db: Session = Depends(db_sync_session)):
    return sv_user.role_post(t=t, db=db)
--- app/v1/services/user.py
+++ app/v1/services/user.py
@@ -1,5 +1,5 @@
 import datetime
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session, selectinload
 from sqlalchemy import select, insert, desc, func, delete
 from app.v1.models import User, Role, IdType
@@ -8,6 +8,19 @@ from app.v1.schemas import user as sc_user
 
 def get_user_all(db: Session = Depends):
    return db.execute(select(User)).scalars().all()
+
+def get_user_by_username(username: str, db: Session = Depends):
+   return db.execute(select(User).filter(User.username==username)).scalar_one_or_none()
+
+def user_patch(t: sc_user.UserPost, db: Session = Depends):
+   user = get_user_by_username(username=t.username, db=db)
+   if user is None:
+      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='User not found')
+   # insert editor, you can get editor from session
+   user.editor = 1
+   for key, value in t.model_dump(exclude_unset=True).items():
+      setattr(user, key, value)
+   return user
 
 def user_post(t: sc_user.UserPost, db: Session = Depends):
    a = db.execute(insert(User).values(username=t.username, password=t.password, name=t.name, email=t.email, role_id=t.role_id, id_type_id=t.id_type_id, id_number=t.id_number, phone=t.phone, address=t.address))
