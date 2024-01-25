9b6a806bb6ea21a801318d583a099717f4eceac0
setiadi1 <edu.setiadi.my@gmail.com>
2024-01-25 21:05:02 +0700

    Add/or update metadata

--- app/main.py
+++ app/main.py
@@ -6,7 +6,17 @@ from app.v1 import router
 
 
 def get_application():
-   _app = FastAPI(title=settings.PROJECT_NAME)
+   _app = FastAPI(
+      title=settings.PROJECT_NAME,
+      summary="A blueprint of a SIMPLE BACKEND (Simbe) with the latest version of FastAPI, Pydantic, SQLAlchemy and Alembic",
+      version="v0.1.0-alpha",
+      terms_of_service="https://github.com/setia1011/simbe",
+      contact={
+         "name": "Setiadi H.",
+         "url": "https://github.com/setia1011/simbe",
+         "email": "setiadi1457@gmail.com",
+      }
+   )
 
    _app.add_middleware(
       CORSMiddleware,
